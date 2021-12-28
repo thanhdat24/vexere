74c772afee44c76b4c35323075ea463452293499
lethanhdatctu <54407533+thanhdat24@users.noreply.github.com>
2021-12-28 22:26:14 +0700

    Upload file multer

--- routers/user.router.js
+++ routers/user.router.js
@@ -1,9 +1,16 @@
 const express = require("express");
 const { register, login } = require("../controllers/user.controllers");
-
 const userRouter = express.Router();
 
 userRouter.post("/register", register);
 userRouter.post("/login", login);
+
+// upload file
+const multer = require("multer");
+const upload = multer({ dest: "uploads/avatars" });
+
+userRouter.post("/upload-avatar", upload.single("avatar"), (req, res) => {
+  res.send("Tính Năng Upload File Run");
+});
 
 module.exports = { userRouter };
