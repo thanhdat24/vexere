2e21ed284cdd406b19e0df0bb1cfa8997b332125
lethanhdatctu <54407533+thanhdat24@users.noreply.github.com>
2021-12-29 16:41:17 +0700

    Relationship station trip

--- models/station.js
+++ models/station.js
@@ -7,8 +7,10 @@ module.exports = (sequelize, DataTypes) => {
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
      */
-    static associate(models) {
+    static associate({ Trip }) {
       // define association here
+      this.hasMany(Trip, { foreignKey: "fromStation" });
+      this.hasMany(Trip, { foreignKey: "toStation" });
     }
   }
   Station.init(
---
+++ models/trip.js
@@ -0,0 +1,27 @@
+"use strict";
+const { Model } = require("sequelize");
+module.exports = (sequelize, DataTypes) => {
+  class Trip extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate({ Station }) {
+      // define association here
+      this.belongsTo(Station, { foreignKey: "fromStation" });
+      this.belongsTo(Station, { foreignKey: "toStation" });
+    }
+  }
+  Trip.init(
+    {
+      startTime: DataTypes.DATE,
+      price: DataTypes.FLOAT,
+    },
+    {
+      sequelize,
+      modelName: "Trip",
+    }
+  );
+  return Trip;
+};
